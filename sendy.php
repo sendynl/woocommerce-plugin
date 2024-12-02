cde110581806ffb3a231dccf0d3bc946f23d65f5
Wim Griffioen <w.griffioen@sendy.nl>
2024-12-02 12:32:48 +0100

    Update the tested up to versions

--- sendy.php
+++ sendy.php
@@ -4,18 +4,18 @@
  * Plugin Name: Sendy
  * Plugin URI: https://app.sendy.nl/
  * Description: A WooCommerce plugin that connects your site to the Sendy platform
- * Version: 3.0
+ * Version: 3.0.1
  * Author: Sendy
  * Author URI: https://sendy.nl/
  * License: MIT
  * Text Domain: sendy
  * Domain Path: /languages
  * Requires at least: 5.2
- * Tested up to: 6.6.2
+ * Tested up to: 6.7.1
  * Requires PHP: 7.4
  * Requires Plugins: woocommerce
  * WC requires at least: 8.2
- * WC tested up to: 9.3.3
+ * WC tested up to: 9.4.2
  *
  * @package Sendy
  */
