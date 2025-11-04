f7ddd2c4ed5fa435165afe0f77e5363bd9f4534f
Wim Griffioen <wim@griffioen.me>
2025-11-04 15:59:15 +0100

    Update the tested up to versions

--- sendy.php
+++ sendy.php
@@ -11,11 +11,11 @@
  * Text Domain: sendy
  * Domain Path: /languages
  * Requires at least: 5.2
- * Tested up to: 6.8
+ * Tested up to: 6.8.3
  * Requires PHP: 7.4
  * Requires Plugins: woocommerce
  * WC requires at least: 8.2
- * WC tested up to: 9.8.2
+ * WC tested up to: 10.3.4
  *
  * @package Sendy
  */
